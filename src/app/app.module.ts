962e6dbee93357b51c5d7336f5104461226d4e16
Valerij Zimnev <sorption.music@gmail.com>
2018-09-25 22:30:06 +0700

    Add components and default styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home/home.component';
+import { PaymentPageComponent } from './payment/payment.component';
+import { NotfoundPageComponent } from './notfound/notfound.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    PaymentPageComponent,
+    NotfoundPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: HomePageComponent, pathMatch: 'full'},
+      { path: 'payment', component: PaymentPageComponent },
+      { path: '404', component: NotfoundPageComponent },
+      { path: '**', redirectTo: '/404' },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/payment/payment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payment',
+  templateUrl: './payment.component.html',
+  styleUrls: ['./payment.component.scss']
+})
+export class PaymentPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
